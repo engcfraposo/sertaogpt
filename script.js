82c0c36a5a9f8d2c19f2e2e04db66a9d029f06f7
Cláudio Rapôso <engcfraposo@gmail.com>
2023-05-20 20:58:16 -0300

    Fist Commit

---
+++ script.js
@@ -0,0 +1,58 @@
+let chatHistory = [];
+
+function sendMessage() {
+    let messageInput = document.getElementById('messageInput');
+    let message = messageInput.value;
+
+     //add user message to screen
+    let chatbox = document.getElementById("chatbox");
+    let userMessage = document.createElement('div');
+    userMessage.className = 'message user-message';
+    userMessage.innerHTML = `<p>${message}</p><span>${new Date()}</span><img class="avatar" src="https://avatars.githubusercontent.com/u/20050327?v=4"></img>`
+    chatbox.appendChild(userMessage)
+
+    chatbox.scrollTop = chatbox.scrollHeight;
+   
+    //Starting loading 
+    document
+    .getElementById('loading')
+    .classList
+    .add('indeterminate');
+
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Authorization': 'Bearer ask-M9NtkDELFccCvOk07A3PT3BlbkFJd4MMwneVYDvJ9a06NJP1'
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": message}]
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        //Finish loading
+        document
+        .getElementById('loading')
+        .className="";
+        
+        let gptResponse = data.choices[0].message.content.trim();
+        let botMessage = document.createElement('div');
+        botMessage.className = 'message bot-message';
+        botMessage.innerHTML = `<img class="avatar" src="assets/bot.png"></img><p>${gptResponse}</p><span>${new Date()}</span>`
+        chatbox.appendChild(botMessage)
+
+        chatbox.scrollTop = chatbox.scrollHeight;
+    })
+    .catch(error => {
+        console.error(error);
+        let errorMessage = document.createElement('div');
+        errorMessage.className = 'message error-message';
+        errorMessage.innerHTML = `<img class="avatar" src="assets/bot.png"></img><p>Erro ao acessar o ChatGPT</p><span>${new Date()}</span>`
+        chatbox.appendChild(errorMessage)
+
+        chatbox.scrollTop = chatbox.scrollHeight;
+    })
+    //alert(JSON.stringify(messageInput.value, null,2))
+}
